fd1e228e20775e7829f07725a26d6dc0e722e14f
Chrilleweb <christian.munknissen@gmail.com>
2024-02-22 01:37:34 +0100

    post, update and delete

--- 03._Drinks_API/app.js
+++ 03._Drinks_API/app.js
@@ -1,32 +1,61 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 const drinks = [
-    { id: 1, name: "Mojito" },
-    { id: 2, name: "Isbjørn" },
-    { id: 3, name: "Old Fashion" },
+  { id: 1, name: "Mojito" },
+  { id: 2, name: "Isbjørn" },
+  { id: 3, name: "Old Fashion" },
 ];
 
 app.get("/drinks", (req, res) => {
-    res.send({ data: drinks });
+  res.send({ data: drinks });
 });
 
 app.get("/drinks/:id", (req, res) => {
-    const providedDrinkId = Number(req.params.id);
-    const foundDrink = drinks.find((drink) => drink.id === providedDrinkId);
-    if (!foundDrink) {
-        res.status(404).send({ data: "Drink not found" });
-    } else {
-        res.send({ data: foundDrink });
-    }
+  const providedDrinkId = Number(req.params.id);
+  const foundDrink = drinks.find((drink) => drink.id === providedDrinkId);
+  if (!foundDrink) {
+    res.status(404).send({ data: "Drink not found" });
+  } else {
+    res.send({ data: foundDrink });
+  }
 });
 
+app.post("/drinks", (req, res) => {
+  const newDrink = req.body;
+  drinks.push(newDrink);
+  res.status(201).send({ data: newDrink });
+});
+
+app.delete("/drinks/:id", (req, res) => {
+  const providedDrinkId = Number(req.params.id);
+  const index = drinks.findIndex((drink) => drink.id === providedDrinkId);
+  if (index === -1) {
+    res.status(404).send({ data: "Drink not found" });
+  } else {
+    drinks.splice(index, 1);
+    res.send({ data: drinks });
+  }
+});
+
+app.put("/drinks/:id", (req, res) => {
+  const providedDrinkId = Number(req.params.id);
+  const index = drinks.findIndex((drink) => drink.id === providedDrinkId);
+  if (index === -1) {
+    res.status(404).send({ data: "Drink not found" });
+  } else {
+    drinks[index] = req.body;
+    res.send({ data: drinks });
+  }
+});
 
 app.listen(8080, (error) => {
-    if (error) {
-        console.log("Error starting the server");
-        return;
-    }
-    console.log("Server is running on port", 8080);
+  if (error) {
+    console.log("Error starting the server");
+    return;
+  }
+  console.log("Server is running on port", 8080);
 });
